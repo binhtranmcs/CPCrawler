358935db184a7cc7b64e8392373b216a8a65a678
binhtranit <binhtran1832@gmail.com>
2020-12-27 15:42:49 +0700

    almost done on crawler

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import requests
-import os
+import os, re
 from bs4 import BeautifulSoup
 
 URL = "https://codeforces.com"
-DIR = './solution'
+SOL = './solution'
+PROB = './problem'
 visited = [0 for i in range(100)]
 problem = {}
 solution = {}
@@ -14,18 +15,14 @@ def visitPage(url):
     text = sourceCode.text
     soup = BeautifulSoup(text, features="html.parser")
     for link in soup.findAll("a"):
-        # print(link)
         href = link.get('href')
         comp = href.split('/')
         if 'page' in comp:
-            continue
             pageNo = int(comp[-1])
             if visited[pageNo] == 0:    
                 visited[pageNo] = 1
-                # print(URL + href)
                 visitPage(URL + href)
         elif len(comp) == 5 and comp[2] == 'problem':
-            continue
             try: 
                 x = problem[href]
             except KeyError:
@@ -40,7 +37,9 @@ def visitPage(url):
 
 def getProblem(url):
     '''eg: url = https://codeforces.com/problemset/problem/1464/F'''
-    f = open('crawler.out', "w")
+    tmp = url.split('/')
+    tmp = '/' + tmp[-2] + tmp[-1]
+    f = open(PROB + tmp + '.txt', "w")
     sourceCode = requests.get(url)
     text = sourceCode.text
     soup = BeautifulSoup(text, features="html.parser")
@@ -54,44 +53,8 @@ def getProblem(url):
             if not i.has_attr('class') or i['class'][0] not in exc:
                 continue
         content = getContent(i)
-        if not i.has_attr('class'):
-            content = ' '.join(content.split()[:10])
-        f.write(getContent(i) + '\n')
-
-    # text = soup.get_text()
-    # lines = (line.strip() for line in text.splitlines())
-    # chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-    # text = '\n'.join(chunk for chunk in chunks if chunk and '\u2192' not in chunk)
-    # res = ""
-    # for i in text:
-    #     if i == '/':
-    #         res += '//'
-    #     else:
-    #         res += i 
-    # f.write(res)
-
-    # tag_list = [i.contents[0] for i in soup.findAll('span', {'class' : 'tag-box'})]
-    # for i in range(len(tag_list)):
-    #     tag_list[i] = getContent(tag_list[i])
-    # f.write("Tag: " + ' '.join(tag_list) + '\n')
-    # f.write('\n')
-
-    # problem = soup.find('div', {'class' : 'problem-statement'})
-    # print("Crawling problem:", url)
-
-    # title = "".join(problem.find('div', {'class' : 'title'}).contents)
-    # f.write(title + '\n')
-    # f.write('\n')
-
-    # time_limit = problem.find('div', {'class' : 'time-limit'}).contents[-1]
-    # f.write("Time limit: " + time_limit + '\n')
-    # mem_limit = problem.find('div', {'class' : 'memory-limit'}).contents[-1]
-    # f.write("Memory limit: " + mem_limit + '\n')
-    # input_file = problem.find('div', {'class' : 'input-file'}).contents[-1]
-    # f.write("Input: " + input_file + '\n')
-    # output_file = problem.find('div', {'class' : 'output-file'}).contents[-1]
-    # f.write("Output: " + output_file + '\n')
-    # f.write('\n')
+        if content not in ['Output\\\\', 'Input\\\\']:
+            f.write(content + '\n\n')
 
 def getSolution(url):
     '''eg: url = https://codeforces.com/problemset/status/1464/problem/F'''
@@ -99,17 +62,50 @@ def getSolution(url):
     source = requests.get(url)
     text = source.text
     soup = BeautifulSoup(text, features = 'html.parser')
+    soup = soup.find('table', {'class' : 'status-frame-datatable'})
+    soup = soup.findAll('tr')[1]
+    language = soup.findAll('td')[4].get_text().strip()
+    ext = getExtension(language)
+
     soup = soup.find('a', {'class' : 'view-source'})
     soup = requests.get(URL + soup.get('href'))
     soup = soup.text 
     soup = BeautifulSoup(soup, features = 'html.parser')
     soup = soup.find('pre')
-    store_code(soup.get_text(), url)
+    store_code(soup.get_text(), url, ext)
+
+def getExtension(s):
+    if 'C++' in s:
+        return '.cpp'
+    if 'C#' in s:
+        return '.cs'
+    if 'Python' in s or 'PyPy' in s:
+        return '.py'
+    if 'C11' in s:
+        return '.c'
+    if 'Java' in s:
+        return '.java'
+    if 'Kotlin' in s:
+        return '.kt'
+    if 'Ruby' in s:
+        return '.rb'
+    if 'Rust' in s:
+        return '.rs'
+    if 'Scala' in s:
+        return '.sc'
+    return '.txt'
 
 def store_code(text, url, extension = '.txt'):
+    '''eg: url = https://codeforces.com/problemset/status/1464/problem/F'''
     tmp = list(url.split('/'))
     tmp = '/' + tmp[-3] + tmp[-1]
-    f = open(DIR + tmp + '.txt', "w")
+    f = open(SOL + tmp + extension, "w")
+    f.write(text)
+
+def store_problem(text, url):
+    tmp = list(url.split('/'))
+    tmp = '/' + tmp[-2] + tmp[-1]
+    f = open(PROB + tmp + '.txt', "w")
     f.write(text)
 
 def getContent(soup):
@@ -119,13 +115,19 @@ def getContent(soup):
     text = '\n'.join(chunk for chunk in chunks if chunk and '\u2192' not in chunk)
     text = "".join((i if i != '/' else '//') for i in text)
     text = ''.join(i for i in text if ord(i) < 128)
-    # text = ' '.join(text.split()[:20])
+    text = '$'.join(text.split('$$$'))
+    text = '\\\\\n'.join(text.split('\n'))
+    text += '\\\\'
     return text
         
 if __name__ == "__main__":
-    if not os.path.exists(DIR):
-        os.makedirs(DIR)
+    if not os.path.exists(SOL):
+        os.makedirs(SOL)
+    if not os.path.exists(PROB):
+        os.makedirs(PROB)
+    
     visitPage(URL + '/problemset')
-    # crawl("https://codeforces.com/problemset/problem/1444/C")
-    # getSolution('https://codeforces.com/problemset/status/1464/problem/F')
+    # getProblem('https://codeforces.com/problemset/problem/1464/F')
+    # getSolution('https://codeforces.com/problemset/status/1468/problem/N')
     # [print(i, end = ',') for i in range(1, 67) if visited[i] == 0]
+
